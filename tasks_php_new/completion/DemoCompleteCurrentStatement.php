33f3381d16f642db4967da312cf697a71263132b
Shaverdova Elena <Elena.Shaverdova@jetbrains.com>
2019-06-24 19:13:42 +0200

    Completion of current statement demo task

---
+++ tasks_php_new/completion/DemoCompleteCurrentStatement.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ *  Complete whole statement with Ctrl+Shift+Enter/Cmd+Shift+Enter
+ */
+$a = new MyCla/*caret*/
+
+class MyClass
+{
+}
+
+class MyClassEx
+{
+}
---
+++ tasks_php_new/completion/DemoCompleteCurrentStatement_.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ *  Complete whole statement with Ctrl+Shift+Enter/Cmd+Shift+Enter
+ */
+$a = new MyClass();
+
+class MyClass
+{
+}
+
+class MyClassEx
+{
+}
